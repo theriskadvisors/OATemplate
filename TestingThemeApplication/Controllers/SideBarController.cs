a531141515a2cabe9ec990030fd04a8463bef60e
Azeem Yousaf <azeemazeem187@gmail.com>
2020-04-16 12:13:09 +0500

    Initial Commit
    Initial Commit of Template SmartAdmin

---
+++ TestingThemeApplication/Controllers/SideBarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingThemeApplication.Controllers
+{
+    public class SideBarController : Controller
+    {
+        // GET: SideBar
+        public ActionResult Index()
+        {
+            return PartialView("_AdminLayout");
+        }
+    }
+}
